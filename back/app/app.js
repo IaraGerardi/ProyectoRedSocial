26c1cd2b774b0189492334f5959dc0d1a1a01800
goncataldo <gonzalo.cataldo@arbusta.net>
2022-08-24 15:11:35 -0300

    estructura app

--- back/app/app.js
+++ back/app/app.js
@@ -3,8 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,8 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require('./routes/router'))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
