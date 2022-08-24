30a20d2bcaa3e0bf5fbc7fbca9cc1e8bee65d662
maxiPortelBeraud <maximiliano.portel@arbusta.net>
2022-08-24 17:21:31 -0300

    Model comments

---
+++ back/app/models/CommentModel.js
@@ -0,0 +1,10 @@
+const db = require('../database/db.js');
+const { DataTypes } = require('sequelize');
+
+const CommentModel = db.define('posts',{
+    content: {types: DataTypes.STRING},
+    likes: {types: DataTypes.INTEGER},
+    image: {types: DataTypes.STRING}
+})
+
+module.exports = CommentModel;
