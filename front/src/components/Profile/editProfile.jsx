7dfca50e574d156d9d8078c53bfb6bb8ef3bd0b6
Arbusta <iara.gerardi@arbusta.net>
2022-08-25 14:39:22 -0300

    actualizaciones perfil

--- front/src/components/Profile/editProfile.jsx
+++ front/src/components/Profile/editProfile.jsx
@@ -2,27 +2,52 @@ import React from "react";
 
 // EditUser es el componente que edita la informacion del usuario, puede cambiar su nombre de usuario, su email y su contraseña, se muestra cuando el usuario da click en el lapiz que aparece al lado de sus "estadisticas"(posts y likes). El componente tambien incluye un div llamado "glassmorphism" para difuminar el fondo cuando se visualice el pop-up.
 
-
-function EditUser({ setEditUser }) {
+// El estado se le pasa desde el padre, tiene que estar false por default y el padre tiene que tener un boton que cambie este estado a true. La x debe cambiar ese estado a falso, y hay que indicarle en que vista se encuentra con el prop "father".
+function EditUser({ setPopUp, father }) {
     return (
         <>
             <div className='editUserForm'>
                 <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAN9JREFUSEvtleENAiEMRt9NoJvoCLqJG6gTOYKucBs4im5gvgQSEu+4VlD8Ab+bPvpoy0CjMzTi0sE/M99V/5XqNXAFzsB94WYH4AjsgUcu1vLGgm1CIiWcgwt6CbAR2JWC04SqYgqexjwDNGvHUrEunoO7oUpoBc/Bt4leU6VRvwc8BVfj6big3orjZVO1H0FrgecaLjt5JaqldxWyu+Ee8FT3ps3lglvBuZGxzPmbdgvYMqduuAUcV+bSyFRfmZrVG3AyfhKK054u/iS+8lVaVHdwFQNddRWNliTNVL8A3XRCH6P6Iy4AAAAASUVORK5CYII="
-                    onClick={() => setEditUser(false)} alt='close edit user' />
-                <form method='PUT'>
-                    <div className='user editBox'>
-                        <label htmlfor='userEdit'>Username</label>
-                        <input type='text' id='userEdit' name='userEdit'></input>
-                    </div>
-                    <div className='email editBox'>
-                        <label htlmfor='emailEdit'>Email</label>
-                        <input type='email' id='emailEdit' name='emailEdit'></input>
-                    </div>
-                    <div className='password editBox'>
-                        <label htlmfor='passwordEdit'>Contraseña</label>
-                        <input type='password' name='passwordEdit' id='passwordEdit'></input>
-                    </div>
-                </form>
+                    onClick={() => setPopUp(false)} alt='close edit user' />
+                {/* Si father es profile renderiza el pop up que tiene que cambiar la informacion del perfil de usuario */}
+                {/* Del perfil se debe poder personalizar:
+                -Foto de perfil y header,
+                -Descripcion
+                -Nombre de usuario, email
+                -Contraseña */}
+                {father === 'profile' ?
+                    <form method='PUT'>
+                        <div className='user editBox'>
+                            <label htmlfor='userEdit'>Username</label>
+                            <input type='text' id='userEdit' name='userEdit'></input>
+                        </div>
+                        <div className='email editBox'>
+                            <label htlmfor='emailEdit'>Email</label>
+                            <input type='email' id='emailEdit' name='emailEdit'></input>
+                        </div>
+                        <div className='password editBox'>
+                            <label htlmfor='passwordEdit'>Contraseña</label>
+                            <input type='password' name='passwordEdit' id='passwordEdit'></input>
+                        </div>
+                    </form>
+                    // Si el father es home renderiza el pop up en el que se crean los posteos
+                    // No tiene ningun estilo pero le puedo poner el mismo estilo que el pop up de editar perfil
+                    : father === 'home' ?
+                        <form method='PUT'>
+                            <div className='user editBox'>
+                                <label htmlfor='userEdit'>Username</label>
+                                <input type='text' id='userEdit' name='userEdit'></input>
+                            </div>
+                            <div className='email editBox'>
+                                <label htlmfor='emailEdit'>Email</label>
+                                <input type='email' id='emailEdit' name='emailEdit'></input>
+                            </div>
+                            <div className='password editBox'>
+                                <label htlmfor='passwordEdit'>Contraseña</label>
+                                <input type='password' name='passwordEdit' id='passwordEdit'></input>
+                            </div>
+                        </form>
+                        : null}
             </div>
             <div className='glassmorphism' />
         </>
