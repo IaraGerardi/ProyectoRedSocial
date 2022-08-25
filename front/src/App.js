4da939bc655c4738250ab23513fce7da554cb084
Arbusta <iara.gerardi@arbusta.net>
2022-08-25 10:17:29 -0300

    Agrego react router

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,28 @@
-/* import Home from './components/Home/Home' */
+/*React y react router */
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/*CSS */
 import './App.css';
+/*Vistas*/
+import Home from './components/Home/Home';
+import Profile from './components/Profile/profile';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Home/> */}
-    </div>
+    <BrowserRouter>
+    <Routes>
+      {/* Cambiar login, allUsers y * cuando vayan haciendo push con los componentes */}
+      {/* <Route path='/login' element={<Login/>}/> */}
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      {/* <Route path='/allUsers' element={users}/> */}
+      {/* <Route path='*' element={<Error/>}/> */}
+    </Routes>
+    {/* <div className="App">
+      <Home/> 
+      <Profile />
+    </div> */}
+    </BrowserRouter>
   );
 }
 
