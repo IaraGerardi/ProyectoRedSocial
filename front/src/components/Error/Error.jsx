e4fe605b3530212dae40c8d45401e975e5e6c53e
Cristian Vera <cristian.vera@arbusta.net>
2022-08-25 15:47:24 -0300

    modificaciones error 404

--- front/src/components/Error/Error.jsx
+++ front/src/components/Error/Error.jsx
@@ -5,7 +5,7 @@ function Error() {
 return (
 
 <div className='containerOne'>
-  <div className='containerTwo'>
+<div className='containerTwo'>
 
   <div className='containerTree'>
        <div><h1 className='hh1'>ERROR 404</h1></div>
@@ -14,12 +14,12 @@ return (
   
   <div className='containerimg'><img className='imgrobot' alt='robot' src={require('../Error/img/robot.png')} /></div>
 
-  <div className='containerFour'>
+<div className='containerFour'>
       <div><h2 className='hh2'>LA PAGINA SOLICITADA NO EXISTE</h2></div>
       <div><button className='botonone'>Inicio</button></div>
-  </div> 
-  </div>
-  
+</div> 
+
+</div> 
 </div>
 
 
