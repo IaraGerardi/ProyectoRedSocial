f8b063c20e9b2fc9bd27cd187de237da9fb95530
Cristian Vera <cristian.vera@arbusta.net>
2022-08-25 14:57:39 -0300

    MODIFICACIONES ERROR 404

--- front/src/components/Error/Error.jsx
+++ front/src/components/Error/Error.jsx
@@ -1,31 +1,27 @@
 import React from 'react'
 import './Error.css';
 
-
-
 function Error() {
-  return (
+return (
 
-    <div className='containerOne'>
-
+<div className='containerOne'>
   <div className='containerTwo'>
 
   <div className='containerTree'>
-       <div><h1>ERROR 404</h1></div>
-       <div><h1>PAGE NOT FOUND</h1></div>
+       <div><h1 className='hh1'>ERROR 404</h1></div>
+       <div><h1 className='hh1'>PAGE NOT FOUND</h1></div>
   </div>
-
-
-       <div className='containerimg'><img className='imgrobot' alt='robot' src={require('../Error/img/robot.png')} /></div>
-       <div><h2>LA PAGINA SOLICITADA NO EXISTE</h2></div>
-       <div><button className='boton'>Inicio</button></div>
-    
-    </div> 
   
+  <div className='containerimg'><img className='imgrobot' alt='robot' src={require('../Error/img/robot.png')} /></div>
+
+  <div className='containerFour'>
+      <div><h2 className='hh2'>LA PAGINA SOLICITADA NO EXISTE</h2></div>
+      <div><button className='botonone'>Inicio</button></div>
+  </div> 
   </div>
- 
-
   
+</div>
+
 
   )
 }
