bab1bdf4e4c214bdfc86d3c558b2fa303a99fe99
Macarena-Leiva <macarena.leiva@arbusta.net>
2022-08-26 17:48:30 -0300

    sidebar

--- front/src/components/Home/components-Home/Sidebar.jsx
+++ front/src/components/Home/components-Home/Sidebar.jsx
@@ -3,11 +3,19 @@
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <ul>
-        <li>Explorar</li>
-        <li>Mensajes</li>
-        <li>Favoritos</li>
-        <li>Cerrar Sesion</li>
+  
+      <ul className="ulSidebar">
+        <li className="liEffect liSidebar"><span className="spanEffect"></span><span className="spanEffect"></span><span className="spanEffect"></span><span className="spanEffect"></span><svg xmlns="http://www.w3.org/2000/svg" focusable="false" id="Outline" viewBox="0 0 24 24" width="34" height="30"><path d="M23.707,22.293l-5.969-5.969a10.016,10.016,0,1,0-1.414,1.414l5.969,5.969a1,1,0,0,0,1.414-1.414ZM10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18Z"/></svg>Explorar</li>
+        <li className="liEffect liSidebar"><span className="spanEffect"></span><span className="spanEffect"></span><span className="spanEffect"></span><span className="spanEffect"></span><svg xmlns="http://www.w3.org/2000/svg" focusable="false" id="Outline" viewBox="0 0 24 24" width="30" height="30"><path d="M19,1H5A5.006,5.006,0,0,0,0,6V18a5.006,5.006,0,0,0,5,5H19a5.006,5.006,0,0,0,5-5V6A5.006,5.006,0,0,0,19,1ZM5,3H19a3,3,0,0,1,2.78,1.887l-7.658,7.659a3.007,3.007,0,0,1-4.244,0L2.22,4.887A3,3,0,0,1,5,3ZM19,21H5a3,3,0,0,1-3-3V7.5L8.464,13.96a5.007,5.007,0,0,0,7.072,0L22,7.5V18A3,3,0,0,1,19,21Z"/></svg>Mensajes</li>
+        <li className="liEffect liSidebar"><span className="spanEffect"></span><span className="spanEffect"></span><span className="spanEffect"></span><span className="spanEffect"></span><svg xmlns="http://www.w3.org/2000/svg" focusable="false" id="Outline" viewBox="0 0 24 24" width="35" height="35"><path d="M23.836,8.794a3.179,3.179,0,0,0-3.067-2.226H16.4L15.073,2.432a3.227,3.227,0,0,0-6.146,0L7.6,6.568H3.231a3.227,3.227,0,0,0-1.9,5.832L4.887,15,3.535,19.187A3.178,3.178,0,0,0,4.719,22.8a3.177,3.177,0,0,0,3.8-.019L12,20.219l3.482,2.559a3.227,3.227,0,0,0,4.983-3.591L19.113,15l3.56-2.6A3.177,3.177,0,0,0,23.836,8.794Zm-2.343,1.991-4.144,3.029a1,1,0,0,0-.362,1.116L18.562,19.8a1.227,1.227,0,0,1-1.895,1.365l-4.075-3a1,1,0,0,0-1.184,0l-4.075,3a1.227,1.227,0,0,1-1.9-1.365L7.013,14.93a1,1,0,0,0-.362-1.116L2.507,10.785a1.227,1.227,0,0,1,.724-2.217h5.1a1,1,0,0,0,.952-.694l1.55-4.831a1.227,1.227,0,0,1,2.336,0l1.55,4.831a1,1,0,0,0,.952.694h5.1a1.227,1.227,0,0,1,.724,2.217Z"/></svg> Favoritos</li>
+        <li >
+        <button className="btnEffectSession">
+  <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" fill="currentColor"></path></svg>
+  <span className="spanSession">cerrar sesion</span>
+</button>
+      
+    </li>
+
       </ul>
     </div>
   )
