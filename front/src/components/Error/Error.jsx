927f089104a5194789ab5a60608d688ddd653530
Cristian Vera <cristian.vera@arbusta.net>
2022-08-26 11:41:06 -0300

    PERFECCIONANDO EL CODIGO

--- front/src/components/Error/Error.jsx
+++ front/src/components/Error/Error.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import './Error.css';
+import React from "react";
+import "./Error.css";
 
 function Error() {
-return (
+  return (
+    <div className="containerOne">
+      <div className="containerTwo">
+        <div className="containerTree">
+          <div>
+            <h1 className="hh1">PAGINA NO ENCONTRADA</h1>
+          </div>
+          <div>
+            <h1 className="hh1">ERROR 404 </h1>
+          </div>
+        </div>
 
-<div className='containerOne'>
-<div className='containerTwo'>
+        <div className="containerimg">
+          <img
+            className="imgrobot"
+            alt="robot"
+            src={require("../Error/img/robot.png")}
+          />
+        </div>
 
-  <div className='containerTree'>
-       <div><h1 className='hh1'>ERROR 404</h1></div>
-       <div><h1 className='hh1'>PAGE NOT FOUND</h1></div>
-  </div>
-  
-  <div className='containerimg'><img className='imgrobot' alt='robot' src={require('../Error/img/robot.png')} /></div>
-
-<div className='containerFour'>
-      <div><h2 className='hh2'>LA PAGINA SOLICITADA NO EXISTE</h2></div>
-      <div><button className='botonone'>Inicio</button></div>
-</div> 
-
-</div> 
-</div>
-
-
-  )
+        <div className="containerFour">
+          <div>
+            <h2 className="hh2">¡LA PAGINA NO EXISTE!</h2>
+          </div>
+          <div>
+            <button className="botonone">Volver para atrás</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Error;
