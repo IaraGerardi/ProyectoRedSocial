b17d539d7798d2e1cd462afb4cab3498ecf3a525
Macarena-Leiva <macarena.leiva@arbusta.net>
2022-08-28 18:22:46 -0300

    btn postear, reset

--- front/src/components/Home/components-Home/BtnPosting.jsx
+++ front/src/components/Home/components-Home/BtnPosting.jsx
@@ -3,8 +3,15 @@ import imgBtnPost from '../assets/editar.png';
 function BtnPosting({OpenWritePost}) {
   return (
     <div className="btnPosting">
-      
-      <button onClick={OpenWritePost}  className="btnPostingChildren"><img src={imgBtnPost} alt="" className='imgBtnPost' />Postear</button>
+        <button onClick={OpenWritePost} className="btnPost1">
+  <div className="svg-wrapper-1">
+    <div className="svg-wrapper">
+    <img src={imgBtnPost} alt="" className='imgBtnPost' />
+    </div>
+  </div>
+  <span className="spanPost">Postear</span>
+</button>
+      {/* <button onClick={OpenWritePost}  className="btnPostingChildren"><img src={imgBtnPost} alt="" className='imgBtnPost' />Postear</button> */}
     </div>
   )
 }
