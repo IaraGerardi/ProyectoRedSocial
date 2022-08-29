097d7e8f6558814348cc4cc41b1e475325326734
Macarena-Leiva <macarena.leiva@arbusta.net>
2022-08-28 23:12:41 -0300

    media query

--- front/src/components/Home/components-Home/Menu.jsx
+++ front/src/components/Home/components-Home/Menu.jsx
@@ -4,7 +4,7 @@ import imgProfile from "../assets/imgUser.jpg";
 function Menu() {
   return (
     <div className="container-Menu">
-      <div className="profile">
+      <div className="profile liSidebar" >
         <img className="imgProfile" src={imgProfile} alt=""/>
     
         <span className="userProfile">@usuario</span>
