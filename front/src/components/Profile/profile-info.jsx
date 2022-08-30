2daf6beeb88437ee54edacb5d1f92da02419c422
maidanaVa <veronica.maidana@arbusta.net>
2022-08-30 12:28:26 -0300

    css profile y home

--- front/src/components/Profile/profile-info.jsx
+++ front/src/components/Profile/profile-info.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImgProfileHeader from './assets/fondoprofile.jpg';
 
 // En este componente esta el icono que abre el popUp (tengo que cambiar el icono)
 // Este componente tiene que tener personalizado el user (nombre), la foto de perfil, el email; tengo que hablar con los de back para ver si pueden agregar header, numero de likes y posts y una descripcion
@@ -17,7 +18,7 @@ function ProfileInfo({ setEdit, setConfigure, userInfo }) {
             <div className="profilePictureContainer">
                 <img className='profilePicture' alt='profilePicture' src='https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80' />
             </div>
-            <img className='headerProfile' src='https://images.unsplash.com/photo-1633596683562-4a47eb4983c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80' alt='headerPhoto' />
+            <img className='headerProfile' src={ImgProfileHeader} alt='headerPhoto' />
             <div className='infoContainer'>
                 <div className='upperInfoProfile'>
                     <p>{userInfo.user}</p>
