0ae7a7b5d542d351039bef967d0bedab70d4cdd7
maidanaVa <veronica.maidana@arbusta.net>
2022-08-30 10:52:44 -0300

    actualizacion

--- front/src/components/Home/components-Home/Menu.jsx
+++ front/src/components/Home/components-Home/Menu.jsx
@@ -1,12 +1,13 @@
 import Sidebar from "./Sidebar";
 import imgProfile from "../assets/imgUser.jpg";
+import { Link } from "react-router-dom";
 
 function Menu() {
   return (
     <div className="container-Menu">
       <div className="profile liSidebar" >
-        <img className="imgProfile" src={imgProfile} alt=""/>
-    
+        <Link to={'/profile/hola'} > <img className="imgProfile" src={imgProfile} alt=""/></Link>
+          
         <span className="userProfile">@usuario</span>
       </div>
      <Sidebar/>
